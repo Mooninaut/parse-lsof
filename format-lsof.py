2418c9599e824b838821f39826877f2feaf69258
Clement Cherlin <ccherlin@dminc.com>
2023-03-09 13:24:12 -0600

    Update Python formatting, add --trunc=n cmdline option

--- format-lsof.py
+++ format-lsof.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 import re
 import subprocess
-import sys
+import math
 from typing import List
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--trunc', type=int, help='Truncate output to N columns', default=-1)
+args = parser.parse_args()
 
 COMMAND = 'c'
 PID = 'p'
@@ -24,17 +29,33 @@ def mergeRuns(numbers: List[int]) -> List[str]:
 
   return [result] + mergeRuns(numbers)
 
+def getCommandLine(pid):
+  f = open(f"/proc/{pid}/cmdline", "r")
+  cmdline = f.read()
+  f.close()
+  cmdline = cmdline.rstrip('\0')
+  if ('\0' in cmdline):
+    return cmdline.split('\0')
+  return cmdline.split()
+
+def getJavaClass(cmdline):
+  for cmd in reversed(cmdline):
+    if re.match(r'^[0-9A-Za-z.]+\.[A-Z][0-9A-Za-z]+$', cmd):
+      return cmd
+  return cmdline[-1]
+
 results = {}
 hostWidth = -1
 
-lines = subprocess.check_output([
+lines = subprocess.run([
   'lsof',
   '+c0',          # Print all characters of command name
   '-iTCP',        # Only list TCP streams
   '-sTCP:LISTEN', # Only list streams in state LISTEN
   '-P',           # Do not convert port numbers to service names
+  '-n',           # Do not convert IP addresses to host names
   '-Fpcn'         # Output machine-readable [F]ormat with [p]rocess IDs, [c]ommand names, and [n]etwork addresses
-]).decode('utf-8').splitlines()
+], capture_output=True, timeout=10).stdout.decode('utf-8').splitlines()
 
 command = ''
 pid = ''
@@ -47,12 +68,19 @@ for line in lines:
   rest = line[1:]
   if prefix == PID:
     pid = rest
+    command = ''
+    host = ''
+    port = 0
+    hostWidth = max(hostWidth, len(pid))
   elif prefix == COMMAND:
     command = rest
     command = re.sub(r'\\x20', ' ', command)
-    command = f"{command}:{pid}"
+
     if not command in results:
       results[command] = {}
+    if not pid in results[command]:
+      results[command][pid] = {}
+
   elif prefix == FID:
     fid = rest # not used
   elif prefix == HOST:
@@ -60,20 +88,48 @@ for line in lines:
     host, port = rest.rsplit(PORT, 1)
     port = int(port)
     hostWidth = max(hostWidth, len(host))
-    if not host in results[command]:
-      results[command][host] = {}
-    results[command][host][port] = True
+    if not host in results[command][pid]:
+      results[command][pid][host] = {}
+    results[command][pid][host][port] = True
 
+# sort case-insensitive, but use case to break ties
 def keyify(k_v):
   command = k_v[0] # tuple of string:dictionary
   return (command.lower(), command) # tuple of string:string
 
-for command, hostDict in sorted(
+lines = [f"{'PID':>{hostWidth}} COMMAND\n{'HOST':>{hostWidth}} PORT(s)"]
+
+for command, cmdDict in sorted(
     results.items(),
-    key = keyify # sort case-insensitive, use case to break ties
-  ):
-  print(command)
-  for host, portDict in sorted(hostDict.items()):
-    print(f"\t%-{hostWidth}s " % host, end='')
-    ports = sorted(portDict.keys())
-    print(*mergeRuns(ports))
+    key = keyify):
+
+  for pid, pidDict in sorted(cmdDict.items()):
+
+    commandLine = getCommandLine(pid)
+
+    cmdPath = commandLine[0]
+    line = f"{int(pid):{hostWidth}d} "
+    if args.trunc > 0 and (len(line) + len(cmdPath)) > args.trunc:
+      shrinkBy = (len(line) + len(cmdPath)) - args.trunc
+      shrinkTo = len(cmdPath) - shrinkBy
+      cmdPath = cmdPath[:math.floor(shrinkTo/2)] + "…" + cmdPath[-math.ceil(shrinkTo/2):]
+
+    line = line + cmdPath
+    lines.append(line)
+
+    if command == "java":
+      line = f"{'Class':>{hostWidth}} "
+      javaClass = getJavaClass(commandLine)
+      if args.trunc > 0 and (len(line) + len(javaClass)) > args.trunc:
+        shrinkBy = (len(line) + len(javaClass)) - args.trunc
+        shrinkTo = len(javaClass) - shrinkBy
+        javaClass = javaClass[:math.floor(shrinkTo/2)] + "…" + javaClass[-math.ceil(shrinkTo/2):]
+      line = line + javaClass
+      lines.append(line)
+
+    for host, portDict in sorted(pidDict.items()):
+      ports = sorted(portDict.keys())
+      lines.append((f"{host:>{hostWidth}} ") + " ".join(mergeRuns(ports)))
+    lines.append("")
+if len(lines) > 0:
+  print("\n".join(lines))
